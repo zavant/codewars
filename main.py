c85eb477adcb9bf870423dd4f0b9a1eb23d61c52
Zachary Avant <zach.avant89@gmail.com>
2021-05-17 22:41:09 -0500

    py-cw

---
+++ main.py
@@ -0,0 +1,15 @@
+#Jaden Case
+
+def to_jaden_case(string):
+    # ...
+    result = ""
+    list_string = string.split()
+    
+    for ele in list_string:
+        if len(result) > 0:
+            result = result + " " + ele.strip().capitalize()
+        else:
+            result = ele.capitalize()
+    return result
+
+print(to_jaden_case('yes I am a healthy man'))
